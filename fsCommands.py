f9954ba83ed7e305a03675cf336a58d9aa986145
Cidayin Nahuat <cidayinn@gmail.com>
2025-05-04 17:48:09 -0600

    added file functions

--- fsCommands.py
+++ fsCommands.py
@@ -33,8 +33,8 @@ def fs_cp(source, destination):
         found = False 
         with open(PFS_FILENAME, "r") as fs:
             for line in fs:
-                if line.startswith("F|") and not line.startswith("X|"):
-                    parts = line.strip().split("|")
+                if line.startswith("F|") and not line.startswith("X|"): #file is not deleted 
+                    parts = line.strip().split("|") #strip removes \n,\r,\t split splits string into a list after '|'
                     if parts[1] == source: #file name match
                         content = parts[-1] #update content 
                         found = True
@@ -46,7 +46,7 @@ def fs_cp(source, destination):
     else: #not a supplemental file 
         try:
             with open(source, "r") as normalFile:
-                content = normalFile.read().strip()
+                content = normalFile.read().strip() #grab every word or char or symbol that is not \n,\r,\t
         except FileNotFoundError:
             print(f"cp error: Normal file '{source}' not found.")
             return
@@ -67,18 +67,55 @@ def fs_show(file):
     """
     Display the content of a supplemental file
     """
-    pass
+    #skip + if it starts with +, else keep file name as is 
+    file = file[1:] if file.startswith("+") else file 
+    with open(PFS_FILENAME, "r") as fs:
+        for line in fs: 
+            if line.startswith("F|") and not line.startswith("X|"):
+                parts = line.strip().split("|")
+                if parts[1] ==  file: #file name match 
+                    print(parts[-1]) #display content 
+                    return 
+    #error message 
+    print(f"show error: File '{file}' not found.")
 
 # Command: merge
-
 def fs_merge(file1, file2, destination):
     """
     Merge contents of two supplemental files into a new one
     """
-    pass
+    #skip + if it starts with +, else keep file name as is 
+    firstFile = file1[1:] if file1.startswith("+") else file1 
+    secondFile = file2[1:] if file2.startswith("+") else file2 
+    #initialize content as empty 
+    contentF1 = contentF2 = ""
+
+    with open(PFS_FILENAME, "r") as fs:
+        lines = fs.readlines() #return as a list of strings 
+    for line in lines:
+        if line.startswith("F") and not line.startswith("X"):
+            parts = line.strip().split("|") #split the read line into an array 
+            if parts[1] == firstFile: #names match -> get content 
+                contentF1 = parts[-1]
+            elif parts[1] == secondFile: #names match -> get content 
+                contentF2 = parts[-1]
+
+    #if empty/not found since no content was added 
+    if not contentF1 or not contentF2:
+        print(f"merge error: one or both files not found.")
+        return 
+
+    mergeContent = contentF1 + contentF2
+    record = f"F|{destination[1:]|{get_timestamp()}|{len(mergeContent)}|{mergeContent}}\n"
+
+    #append to private.pfs
+    with open(PFS_FILENAME, "a") as fs:
+        fs.write(record)
+
+    #success message 
+    print(f"merge: created {destination} with combined contents.")    
 
 # Command: rm
-
 def fs_rm(file):
     """
     Mark a file as deleted in private.pfs
